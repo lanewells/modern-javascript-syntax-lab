7aac7e901e68b6171d9236fc29cfac027bb2fbe5
lanewells <wellsdelaneyk@gmail.com>
2024-10-28 15:17:14 -0700

    Exercise 6 complete

--- modern-javascript-lab/modern-javascript-lab/solutions.js
+++ modern-javascript-lab/modern-javascript-lab/solutions.js
@@ -71,3 +71,18 @@ myCar.model = "q7"
 
 console.log("Original car: ", car1)
 console.log("Cloned car: ", myCar)
+
+// Exercise 6
+
+// Create an object named userProfile.
+// Define a variable named propertyName and assign a string to it (like a username, age, or email).
+// Use propertyName as a dynamic key in userProfile, assigning a relevant value.
+
+// Your code here
+
+const userProfile = {}
+
+const propertyName = "username"
+userProfile[propertyName] = "lanew"
+
+console.log(userProfile)
